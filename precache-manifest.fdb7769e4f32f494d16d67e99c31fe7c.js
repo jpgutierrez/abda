896142cd1bd3683b4a7380dc8c9773486f543c67
Juan Pablo Gutierrez <jpgutierrezalegre@gmail.com>
2020-11-03 22:24:57 -0500

    Updates

---
+++ precache-manifest.fdb7769e4f32f494d16d67e99c31fe7c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8402dec356223c3bfd7ea01aaeb2bb47",
+    "url": "/abda/index.html"
+  },
+  {
+    "revision": "fc250e4419f207dfeac4",
+    "url": "/abda/static/css/main.aef8aeb5.chunk.css"
+  },
+  {
+    "revision": "e61083e3ce28cd77c043",
+    "url": "/abda/static/js/2.0b2ce3c2.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/abda/static/js/2.0b2ce3c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc250e4419f207dfeac4",
+    "url": "/abda/static/js/main.75ae2659.chunk.js"
+  },
+  {
+    "revision": "0fa9bd07f5eb6287bf91",
+    "url": "/abda/static/js/runtime-main.86182943.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/abda/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/abda/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/abda/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/abda/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/abda/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/abda/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/abda/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/abda/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/abda/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/abda/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/abda/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/abda/static/media/illustration-section-02.63d0555d.svg"
+  }
+]);
